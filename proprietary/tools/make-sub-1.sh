0911033a6f01ae3c4d9447811c686783e26f2596
neuralassembly <neuralassembly@gmail.com>
2012-09-07 20:22:16 +0900

    add support for fullcrespo

---
+++ proprietary/tools/make-sub-1.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+SRC_SYSTEM=system
+DST_SYSTEM=../system
+
+cp -a ${SRC_SYSTEM}/etc/permissions/com.google.android.maps.xml ${DST_SYSTEM}/etc/permissions/com.google.android.maps.xml
+cp -a ${SRC_SYSTEM}/etc/permissions/com.google.android.media.effects.xml ${DST_SYSTEM}/etc/permissions/com.google.android.media.effects.xml
+cp -a ${SRC_SYSTEM}/etc/permissions/com.google.widevine.software.drm.xml ${DST_SYSTEM}/etc/permissions/com.google.widevine.software.drm.xml
+cp -a ${SRC_SYSTEM}/etc/permissions/features.xml ${DST_SYSTEM}/etc/permissions/features.xml
+cp -a ${SRC_SYSTEM}/etc/preferred-apps ${DST_SYSTEM}/etc/preferred-apps
+cp -a ${SRC_SYSTEM}/etc/preferred-apps/google.xml ${DST_SYSTEM}/etc/preferred-apps/google.xml
+cp -a ${SRC_SYSTEM}/etc/updatecmds/google_generic_update.txt ${DST_SYSTEM}/etc/updatecmds/google_generic_update.txt
+cp -a ${SRC_SYSTEM}/lib/libearthmobile.so ${DST_SYSTEM}/lib/libearthmobile.so
+cp -a ${SRC_SYSTEM}/lib/libfilterpack_facedetect.so ${DST_SYSTEM}/lib/libfilterpack_facedetect.so
+cp -a ${SRC_SYSTEM}/lib/libfrsdk.so ${DST_SYSTEM}/lib/libfrsdk.so
+cp -a ${SRC_SYSTEM}/lib/libgcomm_jni.so ${DST_SYSTEM}/lib/libgcomm_jni.so
+cp -a ${SRC_SYSTEM}/lib/libgoogle_endpointer_jni.so ${DST_SYSTEM}/lib/libgoogle_endpointer_jni.so
+cp -a ${SRC_SYSTEM}/lib/libpatts_engine_jni_api.so ${DST_SYSTEM}/lib/libpatts_engine_jni_api.so
+cp -a ${SRC_SYSTEM}/lib/libspeexwrapper.so ${DST_SYSTEM}/lib/libspeexwrapper.so
+cp -a ${SRC_SYSTEM}/lib/libvideochat_jni.so ${DST_SYSTEM}/lib/libvideochat_jni.so
+cp -a ${SRC_SYSTEM}/lib/libvideochat_stabilize.so ${DST_SYSTEM}/lib/libvideochat_stabilize.so
+cp -a ${SRC_SYSTEM}/lib/libvoicesearch.so ${DST_SYSTEM}/lib/libvoicesearch.so
+cp -a ${SRC_SYSTEM}/lib/libvorbisencoder.so ${DST_SYSTEM}/lib/libvorbisencoder.so
+cp -a ${SRC_SYSTEM}/media/LMprec_508.emd ${DST_SYSTEM}/media/LMprec_508.emd
+cp -a ${SRC_SYSTEM}/media/PFFprec_600.emd ${DST_SYSTEM}/media/PFFprec_600.emd
+cp -a ${SRC_SYSTEM}/media/audio/ringtones/Radiation.ogg ${DST_SYSTEM}/media/audio/ringtones/Radiation.ogg
+cp -a ${SRC_SYSTEM}/media/audio/ringtones/RadiationOrchestration.ogg ${DST_SYSTEM}/media/audio/ringtones/RadiationOrchestration.ogg
+cp -a ${SRC_SYSTEM}/media/audio/ringtones/RobotsforEveryone.ogg ${DST_SYSTEM}/media/audio/ringtones/RobotsforEveryone.ogg
+cp -a ${SRC_SYSTEM}/media/video/AndroidInSpace.240p.mp4 ${DST_SYSTEM}/media/video/AndroidInSpace.240p.mp4
+cp -a ${SRC_SYSTEM}/media/video/AndroidInSpace.480p.mp4 ${DST_SYSTEM}/media/video/AndroidInSpace.480p.mp4
+cp -a ${SRC_SYSTEM}/media/video/Sunset.240p.mp4 ${DST_SYSTEM}/media/video/Sunset.240p.mp4
+cp -a ${SRC_SYSTEM}/media/video/Sunset.480p.mp4 ${DST_SYSTEM}/media/video/Sunset.480p.mp4
+cp -a ${SRC_SYSTEM}/usr/srec/en-US/endpointer_dictation.config ${DST_SYSTEM}/usr/srec/en-US/endpointer_dictation.config
+cp -a ${SRC_SYSTEM}/usr/srec/en-US/endpointer_voicesearch.config ${DST_SYSTEM}/usr/srec/en-US/endpointer_voicesearch.config
+cp -a ${SRC_SYSTEM}/usr/srec/en-US/ep_acoustic_model ${DST_SYSTEM}/usr/srec/en-US/ep_acoustic_model
+cp -a ${SRC_SYSTEM}/usr/srec/en-US/metadata ${DST_SYSTEM}/usr/srec/en-US/metadata
+cp -a ${SRC_SYSTEM}/vendor/firmware/cypress-touchkey.bin ${DST_SYSTEM}/vendor/firmware/cypress-touchkey.bin
+cp -a ${SRC_SYSTEM}/vendor/lib/libWVStreamControlAPI_L3.so ${DST_SYSTEM}/vendor/lib/libWVStreamControlAPI_L3.so
+cp -a ${SRC_SYSTEM}/vendor/lib/libwvdrm_L3.so ${DST_SYSTEM}/vendor/lib/libwvdrm_L3.so
+cp -a ${SRC_SYSTEM}/media/bootanimation.zip ${DST_SYSTEM}/media/bootanimation.zip
+cp -a ${SRC_SYSTEM}/vendor/lib/libwvm.so ${DST_SYSTEM}/vendor/lib/libwvm.so
+cp -a ${SRC_SYSTEM}/vendor/lib/drm/libdrmwvmplugin.so ${DST_SYSTEM}/vendor/lib/drm/libdrmwvmplugin.so
